86142d8bbb5cfb30f64908ea68c4184337c72bfb
dollyaggarwal <108432161+dollyaggarwal@users.noreply.github.com>
2023-08-30 22:00:29 +0530

    Add files via upload

---
+++ QueueUsingStacks.java
@@ -0,0 +1,11 @@
+package queues;
+import java.util.Stack;
+public class QueueUsingStacks<T> {
+
+	Stack<T>s1;
+	Stack<T>s2;
+	public QueueUsingStacks() {
+		s1=new Stack<>();
+		s2=new Stack<>();
+	}
+}
